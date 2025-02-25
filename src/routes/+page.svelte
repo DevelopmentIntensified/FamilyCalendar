85a8d100abc8a15881605c43c5b205438846bcb7
developmentIntensified <developmentIntensified@gmail.com>
2025-02-24 22:00:07 -0500

    sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 		<img
 			src={bgCalImg}
 			alt="Calendar page"
-			class="absolute top-1 inset-0 box-border h-full w-full object-cover blur-sm"
+			class="absolute inset-0 top-1 box-border h-full w-full object-cover blur-sm"
 		/>
 		<div class="absolute inset-0 m-auto my-10 rounded-lg p-10 lg:w-3/6">
 			<h1 class="mb-1 p-3 font-sans text-5xl font-bold">Keep Your Family's Schedule synced</h1>
@@ -27,33 +27,33 @@
 			<button class="rounded bg-secondary-500 p-3 text-xl font-bold">Get Started for Free</button>
 		</div>
 	</section>
-	<section id="features" class="relative bg-primary-100 px-10 py-3 text-center pb-6">
+	<section id="features" class="relative bg-primary-100 px-10 py-3 pb-6 text-center">
 		<h1 class="mb-1 p-3 font-sans text-2xl font-bold">Features</h1>
 		<div class="px-2">
-			<div class="-mx-2 flex">
-				<div class="w-1/3 px-10">
-					<div class="h-96 rounded-2xl bg-gray-300">
+			<div class="-mx-2 flex flex-auto flex-wrap gap-y-5">
+				<div class="px-10 md:w-1/3">
+					<div class="h-96 rounded-2xl bg-primary-300">
 						<div class="w-full py-10 text-center">
 							<!-- <img src={familyIcon} alt="Family Icon" class="m-auto mb-5 w-64 h-64"/> -->
 							<!-- <h3 class="font-bold text-xl"> Built by Large Families for large families</h3> -->
 							<Calendar class="m-auto mb-5 h-64 w-64" />
-							<h3 class="text-2xl font-bold">A Highly Configurable month view</h3>
+							<h3 class="text-xl font-bold">A Highly Configurable month view</h3>
 						</div>
 					</div>
 				</div>
-				<div class="w-1/3 px-10">
-					<div class="h-96 rounded-2xl bg-gray-300">
+				<div class="px-10 md:w-1/3">
+					<div class="h-96 rounded-2xl bg-primary-300">
 						<div class="w-full py-10 text-center">
 							<img src={todolistIcon} alt="Todo list Icon" class="m-auto mb-5 h-64 w-64" />
-							<h3 class="text-2xl font-bold">Daily todo list</h3>
+							<h3 class="text-xl font-bold">Daily todo list</h3>
 						</div>
 					</div>
 				</div>
-				<div class="w-1/3 px-10">
-					<div class="h-96 rounded-2xl bg-gray-300">
+				<div class="px-10 md:w-1/3">
+					<div class="h-96 rounded-2xl bg-primary-300">
 						<div class="w-full py-10 text-center">
 							<img src={coloredFamilyIcon} alt="Colord Family Icon" class="m-auto mb-5 h-64 w-64" />
-							<h3 class="px-1 text-2xl font-bold">Color Each Family Member's Events</h3>
+							<h3 class="px-1 text-xl font-bold">Color Each Family Member's Events</h3>
 						</div>
 					</div>
 				</div>
