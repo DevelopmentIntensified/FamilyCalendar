93c9b6911eda821c25ca0349530e7fa113dd1ec9
developmentintensified <developmentintensified@gmail.com>
2026-09-05 18:21:14 -0400

    chore(lint): install oxlint anti-slop plugin + config

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,9 @@ import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
+	{
+		ignores: ['tools/oxlint/anti-slop/**']
+	},
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
